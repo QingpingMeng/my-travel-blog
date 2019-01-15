216c1c489c5fb454db22ba62ba0bf91cf2dcd0fc
QingpingMeng <missing1989@gmail.com>
2019-01-14 21:18:04 -0800

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# gatsby-starter-travle-blog
+Gatsby starter for creating a traveler blog
+
+Install this starter (assuming Gatsby is installed) by running from your CLI:
+
+`gatsby new gatsby-blog https://github.com/QingpingMeng/gatsby-start-travel-blog`
+
+Or [view the live demo here](https://traveler-blog.netlify.com/).
+
+## Running in development
+`gatsby develop`
---
+++ src/templates/blog-post.js
@@ -0,0 +1,113 @@
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+import Bio from '../components/bio'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Image from 'gatsby-image'
+import { Section } from '../components/layout';
+import 'github-markdown-css';
+import styled from 'styled-components';
+
+const MarkdownBody = styled.section`
+  box-sizing: border-box;
+  min-width: 200px;
+  max-width: 980px;
+  margin: 0 auto;
+  padding: 45px;
+
+  @media (max-width: 767px) {
+		padding: 15px;
+	}
+`;
+
+
+class BlogPostTemplate extends React.Component {
+  render() {
+    const post = this.props.data.markdownRemark
+    const siteTitle = this.props.data.site.siteMetadata.title
+    const { previous, next } = this.props.pageContext
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        
+        <SEO title={post.frontmatter.title} description={post.excerpt} />
+        <Image fluid={post.frontmatter.coverImage.childImageSharp.fluid} />
+        <MarkdownBody className='markdown-body'>
+        <h1>{post.frontmatter.title}</h1>
+        <p
+          style={{
+            // ...scale(-1 / 5),
+            display: 'block',
+            // marginBottom: rhythm(1),
+            // marginTop: rhythm(-1),
+          }}
+        >
+          {post.frontmatter.date}
+        </p>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        {/* <hr
+          style={{
+            // marginBottom: rhythm(1),
+          }}
+        /> */}
+        {/* <Bio /> */}
+
+        <ul
+          style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'space-between',
+            listStyle: 'none',
+            padding: 0,
+          }}
+        >
+          <li>
+            {
+              previous &&
+              <Link to={previous.fields.slug} rel="prev">
+                ← {previous.frontmatter.title}
+              </Link>
+            }
+          </li>
+          <li>
+            {
+              next &&
+              <Link to={next.fields.slug} rel="next">
+                {next.frontmatter.title} →
+              </Link>
+            }
+          </li>
+        </ul>
+        </MarkdownBody>
+      </Layout>
+    )
+  }
+}
+
+export default BlogPostTemplate
+
+export const pageQuery = graphql`
+  query BlogPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      excerpt(pruneLength: 160)
+      html
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+        coverImage {
+          childImageSharp {
+            fluid(maxHeight: 800, maxWidth: 1920, cropFocus: CENTER, quality: 100) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    }
+  }
+`
